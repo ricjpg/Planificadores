b20d3574862e3421cb3748e8865f990459ee7cb6
Ricardo Guardiola <ricardoguardiolahn@gmail.com>
2023-03-08 22:47:15 -0600

    First commit

---
+++ src/main/java/com/mycompany/planificadores/Planificadores.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.planificadores;
+
+/**
+ *
+ * @author Ricardo Guardiola
+ */
+public class Planificadores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Menu start = new Menu();
+        start.setVisible(true);
+    }
+}
